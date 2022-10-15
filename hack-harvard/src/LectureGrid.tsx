4ea5b2e0e2c803a56ebb39d8cf7aa65a7a1628d5
crackalamoo <37599247+crackalamoo@users.noreply.github.com>
2022-10-15 16:58:50 -0400

    added firebase data to lecturegrid

--- hack-harvard/src/LectureGrid.tsx
+++ hack-harvard/src/LectureGrid.tsx
@@ -1,61 +1,80 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import DropDown from "./DropDown";
 import HomeButton from "./HomeButton";
+import { initializeApp } from "firebase/app";
+import { getDocs, getFirestore } from "firebase/firestore";
+import { collection, query, where, Timestamp } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDPYapXYkdwTze1RvMSwdlBnVf31Hk_7jY",
+  authDomain: "platypus-49047.firebaseapp.com",
+  projectId: "platypus-49047",
+  storageBucket: "platypus-49047.appspot.com",
+  messagingSenderId: "727232881257",
+  appId: "1:727232881257:web:5fc4ab88016e401f2c78f8"
+};
+const firebaseapp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseapp);
+
+const fetchData = {
+  "university": "Brown",
+  "class": "APMA1650"
+};
+
+function format_date(date) {
+  return ''+date.seconds;
+}
+function format_lecture(doc) {
+  const data = doc.data();
+  return {
+    title: data.title,
+    date: format_date(data.date),
+    key: data.id
+  };
+}
 
 function LectureGrid() {
-  const [count, setCount] = useState(0);
-  const options = {
-    role: ["Student", "Professor"],
-    class: ["APMA 1650", "APMA 1651", "APMA 1652"],
-    university: ["Brown", "Harvard", "MIT"],
-    action: ["Record", "Watch"],
-  };
 
-  const lectures = [
-    {
-      title: "Lecture 1",
-      id: "1",
-      class: "APMA 1650",
-      date: "2021-01-01",
-      professor: "Professor 1",
+  const [docs, setDocs] = useState([]);
 
-    },
-    {
-      title: "Lecture 2",
-      id: "2",
-      class: "APMA 1650",
-      date: "2021-01-01",
-      professor: "Professor 1",
-    },
-    {
-      title: "Lecture 3",
-      id: "3",
-      class: "APMA 1650",
-      date: "2021-01-01",
-      professor: "Professor 1",
-    },
-  ]
+  useEffect(() => {
+    async function getData() {
+    const q = query(collection(db, "lectures"),
+      where("university", "==", fetchData.university),
+      where("class", "==", fetchData.class));
+    
+    getDocs(q).then((foundDocs) => {
+
+      const update_docs = [];
+      console.log(foundDocs);
+    
+    foundDocs.forEach((doc) => {
+      update_docs.push(doc);
+    });
+
+    setDocs(update_docs.map(format_lecture));
+    console.log(update_docs.map(format_lecture));
+    });
+  }
+  getData();
+}, []);
 
   return (
     <div className="w-screen p-10">
         <ul id="headers" className=" gap-3 font-bold text-2xl mt-8 grid grid-cols-4 text-left border bg-black">
             <li className="p-4 text-white">Lecture</li>
-            <li className="p-4 text-white">Class</li>
             <li className="p-4 text-white">Date</li>
-            <li className="p-4 text-white">Professor</li>
         </ul>
         <hr></hr>
         <ul>
-            {lectures.map((lecture) => {
+            {docs.map((lecture) => {
 
                 return (
                     <a href={`watch/lecture-${lecture.id}`} className="w-full gap-3 grid-cols-4 grid text-left border">
                         <li className="p-4">{lecture.title}</li>
-                        <li className="p-4">{lecture.class}</li>
                         <li className="p-4">{lecture.date}</li>
-                        <li className="p-4">{lecture.professor}</li>
                     </a>
                 )
             })}
