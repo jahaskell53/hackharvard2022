ae5bf1b9d8a142e17c758df2482e0b0aee2cf91d
Jakobi Haskell <jakobi_haskell@brown.edu>
2022-10-15 00:26:16 -0400

    hi

--- hack-harvard/src/App.tsx
+++ hack-harvard/src/App.tsx
@@ -6,8 +6,14 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-    I am a student at Brown University in looking to 
+    <div className="App text-4xl">
+    I am a student at Brown University in looking to
+  <select name="cars" id="cars" className="outline-none bg-white">
+    <option value="volvo">Volvo</option>
+    <option value="saab">Saab</option>
+    <option value="opel">Opel</option>
+    <option value="audi">Audi</option>
+  </select>
     </div>
   )
 }
