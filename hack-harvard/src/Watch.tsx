e16836a834ca29cb616a872745d1acecfb7fbfc1
Jakobi Haskell <jakobi_haskell@brown.edu>
2022-10-15 01:48:28 -0400

    routing works

--- hack-harvard/src/Watch.tsx
+++ hack-harvard/src/Watch.tsx
@@ -3,7 +3,7 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 import DropDown from "./DropDown";
 
-function Record() {
+function Watch() {
   const [count, setCount] = useState(0);
   const options = {
     role: ["Student", "Professor"],
@@ -13,8 +13,8 @@ function Record() {
   };
 
   return (
-   <h1>Record</h1>
+    <h1>Test Watch</h1>
   );
 }
 
-export default Record;
+export default Watch;
