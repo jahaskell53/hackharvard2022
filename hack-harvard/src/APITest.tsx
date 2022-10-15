2e8da4aac84fa45c5b6304cd433f7e4d08aaae43
crackalamoo <37599247+crackalamoo@users.noreply.github.com>
2022-10-15 14:50:29 -0400

    bad commit

--- hack-harvard/src/APITest.tsx
+++ hack-harvard/src/APITest.tsx
@@ -4,7 +4,8 @@ import "./App.css";
 import axios from "axios"
 import { initializeApp } from "firebase/app";
 import { getDocs, getFirestore } from "firebase/firestore";
-import { collection, addDoc, query, where, Timestamp } from "firebase/firestore"; 
+import { collection, addDoc, query, where, Timestamp } from "firebase/firestore";
+import { getStorage, ref, uploadBytes } from "firebase/storage";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDPYapXYkdwTze1RvMSwdlBnVf31Hk_7jY",
@@ -16,63 +17,112 @@ const firebaseConfig = {
 };
 const firebaseapp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseapp);
+const storage = getStorage(firebaseapp);
 
-const UPLOAD = false;
+const assembly = axios.create({
+    baseURL: "https://api.assemblyai.com/v2",
+    headers: {
+        authorization: "0daee8a3236348678195040d20b89b83",
+        "content-type": "application/json",
+    },
+});
+
+const UPLOAD = true;
+
+function recordAudio() {
+    navigator.mediaDevices.getUserMedia({audio: true})
+    .then(stream => {
+        const recorder = new MediaRecorder(stream);
+        recorder.start();
+        let audioChunks = [];
+        recorder.addEventListener("dataavailable", event => {
+            audioChunks.push(event.data);
+        });
+
+        setTimeout(() => {
+            recorder.stop();
+            console.log("Stopped recording");
+        }, 8000);
+
+        setTimeout(() => {
+            const audioBlob = new Blob(audioChunks, {type: 'audio/m4a'});
+            console.log(audioBlob);
+            const audioUrl = URL.createObjectURL(audioBlob);
+            const audio = new Audio(audioUrl);
+            audio.play();
+            uploadAudio(audioBlob);
+        }, 8100);
+    })
+}
+
+function uploadAudio(blob : Blob) {
+
+    const recordClass = "APMA1650";
+    const storageRef = ref(storage, 'lecturetest.m4a');
+    const audioMetadata = {
+        contentType: 'audio/m4a'
+    };
+    console.log(blob);
+    //storageRef.put(blob).then((snapshot) => {
+    //uploadBytes(storageRef, new File([blob], "lecturetest.mp3", {type: blob.type}), audioMetadata).then((snapshot) => {
+    //console.log(snapshot);
+
+    assembly
+        .post("/transcript", {
+            audio_url: "https://bit.ly/3yxKEIY"
+            //audio_url: URL.createObjectURL(blob)
+        })
+        .then((res) => {
+            console.log(res.data.id);
+            let dateObj = new Date();
+            const defaultTitle = recordClass+" "+
+                (dateObj.getMonth()+1)+'/'+dateObj.getDate()+'/'+dateObj.getFullYear()
+                + " " + dateObj.getHours()+":"+dateObj.getMinutes();
+            const docData = {
+                "university": "Brown",
+                "class": "APMA1650",
+                "title": defaultTitle,
+                "id": res.data.id,
+                "date": Timestamp.now()
+            };
+            addDoc(collection(db, "lectures"), docData);
+        })
+        .catch((err) => console.error(err));
+}
+
+function playAudio() {
+
+    /*const fetchData = {
+        "university": "Brown",
+        "class": "APMA1650",
+        "title": "APMA1650 10/15/2022:11:55"
+    };
+    const q = query(collection(db, "lectures"),
+        where("university", "==", fetchData.university),
+        where("class", "==", fetchData.class),
+        where("title", "==", fetchData.title));
+    var foundLectures = 0;
+    getDocs(q).then((foundDocs) => {
+    foundDocs.forEach((doc) => {
+        foundLectures++;
+        if (foundLectures == 1) { */
+            //const transcriptId = doc.data().id;
+            const transcriptId = "rkw6birfmm-2859-46e0-bd64-5fc9005d0ed2";
+            assembly
+                .get("/transcript/"+transcriptId)
+                .then((res) => console.log(res.data.text))
+                .catch((err) => console.error(err));
+        /*}
+    })
+    });*/
+}
 
 function APITest() {
     
-    const assembly = axios.create({
-        baseURL: "https://api.assemblyai.com/v2",
-        headers: {
-            authorization: "0daee8a3236348678195040d20b89b83",
-            "content-type": "application/json",
-        },
-    });
     if (UPLOAD) {
-        assembly
-            .post("/transcript", {
-                audio_url: "https://bit.ly/3yxKEIY"
-            })
-            .then((res) => {
-                console.log(res.data.id);
-                var recordClass = "APMA1650";
-                var dateObj = new Date();
-                const defaultTitle = recordClass+" "+
-                    (dateObj.getMonth()+1)+'/'+dateObj.getDate()+'/'+dateObj.getFullYear()
-                    + ":" + dateObj.getHours()+":"+dateObj.getMinutes();
-                const docData = {
-                    "university": "Brown",
-                    "class": "APMA1650",
-                    "title": defaultTitle,
-                    "id": res.data.id,
-                    "date": Timestamp.now()
-                };
-                addDoc(collection(db, "lectures"), docData);
-            })
-            .catch((err) => console.error(err));
+        recordAudio();
     } else {
-        const fetchData = {
-            "university": "Brown",
-            "class": "APMA1650",
-            "title": "APMA1650 10/15/2022:11:55"
-        };
-        const q = query(collection(db, "lectures"),
-            where("university", "==", fetchData.university),
-            where("class", "==", fetchData.class),
-            where("title", "==", fetchData.title));
-        var foundLectures = 0;
-        getDocs(q).then((foundDocs) => {
-        foundDocs.forEach((doc) => {
-            foundLectures++;
-            if (foundLectures == 1) {
-                const transcriptId = doc.data().id;
-                assembly
-                    .get("/transcript/"+transcriptId)
-                    .then((res) => console.log(res.data.text))
-                    .catch((err) => console.error(err));
-            }
-        })
-    })
+        playAudio();
     }
 
   return (
