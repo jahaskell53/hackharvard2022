0bec04899e8d6a4363b9c61f25cac29cb5eac1f5
shamelsameer <i-shamel.sameer@devrev.ai>
2022-10-16 01:12:40 -0400

    feat: recap leads to home

--- hack-harvard/src/components/Navbar.tsx
+++ hack-harvard/src/components/Navbar.tsx
@@ -6,7 +6,7 @@ export function Navbar() {
         <div className="container-fluid w-full flex flex-wrap items-center justify-between px-6">
             <div className="container-fluid" id="navbar_recap">
             
-            <a className="text-xl text-white font-semibold" href="#"><img src="/src/assets/recap_transparent.png" id="navbaricon"></img>Recap</a>
+            <a className="text-xl text-white font-semibold" href="/"><img src="/src/assets/recap_transparent.png" id="navbaricon"></img>Recap</a>
             </div>
             <HomeButton />
         </div>
