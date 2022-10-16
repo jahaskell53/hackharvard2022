cd7cb771e120cf7af48a96e9362e872ae317b92e
crackalamoo <37599247+crackalamoo@users.noreply.github.com>
2022-10-16 00:45:50 -0400

    update navbar

--- hack-harvard/src/components/Navbar.tsx
+++ hack-harvard/src/components/Navbar.tsx
@@ -4,8 +4,9 @@ export function Navbar() {
     return (
         <nav className="relative w-screen flex flex-wrap items-center justify-between py-3 bg-indigo-600 text-gray-500 hover:text-gray-700 focus:text-gray-700 shadow-lg">
         <div className="container-fluid w-full flex flex-wrap items-center justify-between px-6">
-            <div className="container-fluid">
-            <a className="text-xl text-white font-semibold" href="#">Lecture Capture AI</a>
+            <div className="container-fluid" id="navbar_recap">
+            
+            <a className="text-xl text-white font-semibold" href="#"><img src="/src/assets/recap_transparent.png" id="navbaricon"></img>Recap</a>
             </div>
             <HomeButton />
         </div>
